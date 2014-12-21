d4cf0f45528dee35359ca152971ad4ae45b67fcd
kenncheng <kenncheng@gmail.com>
2014-12-21 17:21:24 +0800

    edit
    edit plot 3 and 4

--- plot3.R
+++ plot3.R
@@ -9,14 +9,13 @@ NEI <- readRDS("./EDA/Project_2/summarySCC_PM25.rds")
 
 # Take subset of data to include only Baltimore City, MD (fips == '24510')
 MD <- subset(NEI, fips=='24510')
-MD$year <- factor(MD$year, levels=c('1999', '2002', '2005', '2008'))
 
 # Generate plot into filename plot3.png
 png(filename = "./EDA/Project_2/plot3.png")
 
-ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
-        geom_boxplot(aes(fill=type)) + stat_boxplot(geom ='errorbar') +
-        ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) + xlab('Year') + 
-        ggtitle('Emissions per Type in Baltimore City, Maryland') 
+ggplot(MD, aes(year, Emissions, color = type)) +
+        geom_line(stat = "summary", fun.y = "sum") +
+        ylab(expression('Total PM'[2.5]*" Emissions")) + xlab('Year') +
+        ggtitle("Total Emissions per Type in Baltimore City")
 
 dev.off()
--- plot4.R
+++ plot4.R
@@ -21,7 +21,7 @@ png(filename = "./EDA/Project_2/plot4.png")
 
 ggplot(data=merge.sum, aes(x=Year, y=Emissions/1000)) + 
         geom_line(aes(group=1, col=Emissions)) + geom_point(aes(size=2, col=Emissions)) + 
-        ggtitle(expression('Total Emissions of PM'[2.5])) + 
+        ggtitle(expression('Total Emissions of PM'[2.5]*" from coal related sources")) + 
         ylab(expression(paste('PM', ''[2.5], ' in kilotons'))) + 
         geom_text(aes(label=round(Emissions/1000,digits=2), size=2, hjust=1.5, vjust=1.5)) + 
         theme(legend.position='none') + scale_colour_gradient(low='black', high='blue')
